394dca9f8422d684fbf53923e00fa6d269ee79dd
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-21 15:23:38 +0600

    added replace

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ document.querySelector('select').addEventListener('input', function(){
   document.querySelectorAll('ul').forEach(
    ul => ul.classList.toggle('hidden', this.value !== ul.id)
   );
-}); //rth
+}); 
+
+document.body.classList.add('hello');
+document.body.classList.remove('hello');
+
+document.body.classList.toggle('hello');
+document.body.classList.toggle('hello', 4 > 4);
+document.body.classList.replace('hello', 'bye');
+if (document.body.classList.contains('bye')){
+
+}
+
+document

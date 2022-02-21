afc291f45faa1b0cd1a836a74b3daaf62f447171
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-21 15:18:06 +0600

    added class list

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ document.querySelector('select').addEventListener('input', function(){
   document.querySelectorAll('ul').forEach(
    ul => ul.classList.toggle('hidden', this.value !== ul.id)
   );
-});
+}); //

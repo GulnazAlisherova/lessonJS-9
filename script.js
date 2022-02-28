6cf8075a8c98d0a28b75549e859e97d9f56910df
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-28 13:32:22 +0600

    added addEventListner

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ document.getElementById('art2').addEventListener('click',function(){
 });
 
 let article3 = document.getElementById('article3');
-article3.style.display = "none"; // сти
+article3.style.display = "none"; // стил
 document.getElementById('art3').addEventListener('dblclick',function(){
   article3.style.display = "block";
 });

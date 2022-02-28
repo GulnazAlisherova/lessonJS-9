2d2ba319d4279804895368d0fcb9dba3dd6d758a
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-28 13:27:48 +0600

    added style.display

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ article3.style.display = "none";
 document.getElementById('art3').addEventListener('dblclick',function(){
   article3.style.display = "block";
 });
-document.getElementById('art3').addEventListener('click',function(){ // по  id вы
+document.getElementById('art3').addEventListener('click',function(){ // по  id выб
   article3.style.display = "none";
 });
 
